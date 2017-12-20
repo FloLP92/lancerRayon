5b8bcf80af2309182b7ef3cca7cb1a36f5724593
FloLP <florianlepallec@gmail.com>
2017-12-20 18:04:41 +0100

    scene

---
+++ lancerRayon/src/Scene.h
@@ -0,0 +1,27 @@
+#ifndef SCENE_H_
+#define SCENE_H_
+#include <vector>
+#include <iostream>
+//#include <boost/optional.hpp>
+#include "Sphere.h"
+#include "Light.h"
+#include "Screen.h"
+
+class Scene {
+private:
+	std::vector<Sphere> tabSphere;
+	Light light;
+	Coord3 camera;
+	Screen screen;
+public:
+	Scene();
+	virtual ~Scene();
+	Screen getScreen();
+	void setScreen(Screen s);
+	Light getLight();
+	void setLight(Light l);
+	void lecture();
+	void write_image();
+};
+
+#endif /* SCENE_H_ */
