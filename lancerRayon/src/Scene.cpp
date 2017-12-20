04597191a1468ee44c47e8dc12255e55af6544e1
vinnvi <vin.doisneau@free.fr>
2017-12-20 23:14:28 +0100

    comments scene

--- lancerRayon/src/Scene.cpp
+++ lancerRayon/src/Scene.cpp
@@ -43,45 +43,44 @@ void lecture(){
 	   	float aF,bF,cF,dF,eF,fF,gF,hF;
 	    if (!(iss >> a)) { break; } // error
 	    else if(a.compare("#")!=0){ // on lit une donnée
-	    	dataRead++;
-	    	//aP = atoi(a.c_str());
-	    	aP = a;
-	    	aP.erase (std::remove(aP.begin(), aP.end(),','), aP.end());
+	    	dataRead++; // On incremente le nombre de données utiles lues
+	    	aP = a; // on place la donnee dans notre liste des donnees lues
+	    	aP.erase (std::remove(aP.begin(), aP.end(),','), aP.end()); //on supprimes les , de la ligne
 	    	switch(dataRead){
-	    		case 1: //Camera position
+	    		case 1: //Camera position (on lit 2 autres données sur cette ligne)
+	    			iss >> bP >> cP; // on place le reste de la lecture dans des variables
+	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());//on supprimes les , de la ligne
+	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());//on supprimes les , de la ligne
+	    			cout << aP << " " << bP << " " << cP << endl;
+	    			break;
+	    		case 2: //Top left corner (on lit 2 autres donnees sur cette ligne)
 	    			iss >> bP >> cP;
 	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());// remove commas
 	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
 	    			cout << aP << " " << bP << " " << cP << endl;
 	    			break;
-	    		case 2: //Top left corner
+	    		case 3: //Top right corner (on lit 2 autres donnees sur cette ligne)
 	    			iss >> bP >> cP;
 	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());// remove commas
 	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
 	    			cout << aP << " " << bP << " " << cP << endl;
 	    			break;
-	    		case 3: //Top right corner
+	    		case 4 : // Bottom left corner (on lit 2 autres donnees sur cette ligne)
 	    			iss >> bP >> cP;
 	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());// remove commas
 	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
 	    			cout << aP << " " << bP << " " << cP << endl;
 	    			break;
-	    		case 4 : // Bottom left corner
-	    			iss >> bP >> cP;
-	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());// remove commas
-	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
-	    			cout << aP << " " << bP << " " << cP << endl;
-	    			break;
-	    		case 5 : // screen horizontal resolution
+	    		case 5 : // screen horizontal resolution (on lit 1 autre donnee sur cette ligne)
 	    			cout << aP << endl;
 	    			break;
-	    		case 6 : // background color
+	    		case 6 : // background color (on lit 2 autres donnees sur cette ligne
 	    			iss >> bP >> cP;
 	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());// remove commas
 	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
 	    			cout << aP << " " << bP << " " << cP << endl;
 	    			break;
-	    		case 7 : // Light source position and color
+	    		case 7 : // Light source position and color on lit 5 autres donnees
 	    			iss >> bP >> cP >> dP >> eP >> fP;
 	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());// remove commas
 	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
@@ -96,8 +95,8 @@ void lecture(){
 	    			fF = strtof(fP.c_str(),0);
 	    			cout << aP << " " << bP << " " << cP << " " << dP << " " << eP << " " << fP <<endl;
 	    			break;
-	    		default : //Create Circles (just them for the moment)
-	    			iss >> infos >> bP >> cP >> dP >> eP >> fP >> gP >> hP;
+	    		default : //reste des infos utiles cad la liste des cercles en bas du fichier
+	    			iss >> infos >> bP >> cP >> dP >> eP >> fP >> gP >> hP; //8 donnees utiles
 	    			infos.erase (std::remove(infos.begin(), infos.end(),','), infos.end());
 	    			bP.erase (std::remove(bP.begin(), bP.end(),','), bP.end());
 	    			cP.erase (std::remove(cP.begin(), cP.end(),','), cP.end());
